363928bbd8739346260a8f56f801ef9320ee5af7
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-01-17 14:07:00 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,5 +1,13 @@
+from game import Game
+import random
 
 class AI:
 
     def find_legal_move(self, g, func, timeout=None):
-        pass
+        move = random.choice(range(7))
+        if g.board[0][move] == g.BLANK:
+            func(move)
+        else:
+            if g.number_of_moves == g.max_moves:
+                raise Exception("DRAW")
+            self.find_legal_move(g, func)
