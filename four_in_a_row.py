5025eed0e4ee09c512bb39519bb122f539d85930
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-01-17 14:06:41 +0200

    Update four_in_a_row.py

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,5 +1,34 @@
-import game
+from game import Game
 import communicator
-import ai
+from ai import AI
 
 
+game1 = Game()
+ai = AI()
+game1.__repr__()
+player1 = False
+player2 = False
+while True:
+    if player1 != player2:
+        if not player1:
+            ai_player = game1.PLAYER_ONE
+        if not player2:
+            ai_player = game1.PLAYER_TWO
+        if game1.get_current_player() == ai_player:
+            ai.find_legal_move(game1, game1.make_move)
+        else:
+            game1.make_move(int(input("column")))
+        game1.__repr__()
+        game1.get_winner()
+        game1.switch_player()
+    else:
+        if player1:
+            game1.make_move(int(input("column")))
+            game1.__repr__()
+            game1.get_winner()
+            game1.switch_player()
+        else:
+            ai.find_legal_move(game1, game1.make_move)
+            game1.__repr__()
+            game1.get_winner()
+            game1.switch_player()
