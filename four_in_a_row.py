2a73e939cbe185e705a45686cfd5c09b9a099726
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-01-18 22:11:56 +0200

    Update four_in_a_row.py

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -6,13 +6,13 @@ from ai import AI
 game1 = Game()
 ai = AI()
 game1.__repr__()
-player1 = False
+player1 = True
 player2 = False
 while True:
     if player1 != player2:
         if not player1:
             ai_player = game1.PLAYER_ONE
-        if not player2:
+        else:
             ai_player = game1.PLAYER_TWO
         if game1.get_current_player() == ai_player:
             ai.find_legal_move(game1, game1.make_move)
