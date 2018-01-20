ea2f0344fb09e6a8e65e05269b52ba9c5f0f88c2
jasgreenspan <34744366+jasgreenspan@users.noreply.github.com>
2018-01-20 22:53:18 +0200

    Update four_in_a_row.py

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -3,24 +3,30 @@ import communicator
 from game import Game
 from ai import AI
 
-COLUMNS = 7
-ROWS = 6
+BOARD_COLUMNS = 7
+BOARD_ROWS = 6
+BUTTONS_ROW = 1
 PLAYER_1 = 0
 PLAYER_2 = 1
+P1_WIN_MSG = "Red wins!"
+P2_WIN_MSG = "Yellow wins!"
+DRAW_MSG = "It's a draw!"
 
 class GameBoard:
     def __init__(self, root, game):
         self._root = root
         self._game = game
+        self._buttons = []
         
-        for i in range(ROWS):
-            for j in range(COLUMNS):
+        for i in range(BOARD_ROWS + BUTTONS_ROW):
+            for j in range(BOARD_COLUMNS):
                 # Row of buttons to add piece in respective column.
                 if i == 0:
-                    self._add_button = tki.Button(self._root, height = 1,
-                                                  width = 12,
+                    self._button = tki.Button(self._root, text = j,
+                                                  height = 1, width = 10,
                                             command = self._make_button(j))
-                    self._add_button.grid(row = i, column = j)
+                    self._button.grid(row = i, column = j)
+                    self._buttons.append(self._button)
                 # Add blank tiles on rest of board.
                 else:
                     tile = tki.Label(root, image = background_tile_img,
@@ -29,45 +35,69 @@ class GameBoard:
                     tile.grid(row = i, column = j)
     
     def _make_button(self, column_coord):
+        
         def _add_piece():
-            x_coord, y_coord = self._game.make_move(column_coord)
-            self._game.switch_player()
-            self._game.get_winner()
+            
+            # Get coordinates of new move (if legal) and adjust to board with
+            # buttons row.
+            y_coord, x_coord = self._game.make_move(column_coord)
+            y_coord += BUTTONS_ROW
+            
             # Check which player added the piece and add respective piece.
             if self._game.current_player == PLAYER_1:
                 player_1_piece = tki.Label(root, image = player_1_piece_img,
-                                           bg="blue", borderwidth = 0))
+                                           bg="blue", borderwidth = 0)
                 player_1_piece.image = player_1_piece_img
-                player_1_piece.grid(row = x_coord, column = y_coord)
+                player_1_piece.grid(row = y_coord, column = x_coord)
             elif self._game.current_player == PLAYER_2:
                 player_2_piece = tki.Label(root, image = player_2_piece_img,
-                                           bg="blue", borderwidth = 0))
+                                           bg="blue", borderwidth = 0)
                 player_2_piece.image = player_2_piece_img
-                player_2_piece.grid(row = x_coord, column = y_coord)
-        
+                player_2_piece.grid(row = y_coord, column = x_coord)
+                
+            # Check for winner and end game if there is one (or draw).
+            winner = self._game.get_winner()
+            if winner is not None:
+                if winner == self._game.DRAW:
+                    self._end_message(DRAW_MSG)
+                elif winner == PLAYER_1:
+                    self._end_message(P1_WIN_MSG)
+                elif winner == PLAYER_2:
+                    self._end_message(P2_WIN_MSG)         
+            self._game.switch_player()
+            
         return _add_piece
         
-    def end_msg(self):
-        pass
-    
+    def _end_message(self, message):
+        self._message_box = tki.Toplevel()
+        message_display = tki.Entry(self._message_box, justify="center",
+                                    width=50)
+        message_display.pack()
+        message_display.insert(0, message)
+        # Disable any more pieces being added to the board.
+        for button in self._buttons:
+            button.config(state="disabled")
+
 if __name__ == "__main__":
     game1 = Game()
     ai = AI()
     root = tki.Tk()
     
+    coin_img = tki.PhotoImage(file = "coin_img.gif")
     background_tile_img = tki.PhotoImage(file = "blank_tile.gif")
     player_1_piece_img = tki.PhotoImage(file = "red_piece.gif")
     player_2_piece_img = tki.PhotoImage(file = "yellow_piece.gif")
-    
+
     GameBoard(root, game1)
     root.mainloop()
-   
-
+    
+#player1 = False
+#player2 = False
 #while True:
 #    if player1 != player2:
 #        if not player1:
 #            ai_player = game1.PLAYER_ONE
-#        else:
+#        if not player2:
 #            ai_player = game1.PLAYER_TWO
 #        if game1.get_current_player() == ai_player:
 #            ai.find_legal_move(game1, game1.make_move)
